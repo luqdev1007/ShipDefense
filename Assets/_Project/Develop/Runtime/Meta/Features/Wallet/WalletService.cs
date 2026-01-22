2b469f5b0cd058f1f4a4390775ed4cdc0c580033
LuQmu5 <luqdev1007@gmail.com>
2026-01-22 19:14:47 +0400

    UI, gameplay

--- Assets/_Project/Develop/Runtime/Meta/Features/Wallet/WalletService.cs
+++ Assets/_Project/Develop/Runtime/Meta/Features/Wallet/WalletService.cs
@@ -57,8 +57,6 @@ namespace Assets._Project.Develop.Runtime.Meta.Features.Wallet
 
         public void ReadFrom(PlayerData data)
         {
-            Debug.Log(data.WalletData == null);
-
             foreach (KeyValuePair<CurrencyTypes, int> currency in data.WalletData)
             {
                 if (_currencies.ContainsKey(currency.Key))
