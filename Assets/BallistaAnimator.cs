0b1e2204d3deb3a1a4793ee503161681e48ebb47
LuQmu5 <luqdev1007@gmail.com>
2026-01-23 22:42:41 +0400

    physics test

--- Assets/BallistaAnimator.cs
+++ Assets/BallistaAnimator.cs
@@ -8,6 +8,8 @@ public class BallistaAnimator : MonoBehaviour
     [SerializeField] private Transform _backMechRoll;
     [SerializeField] private Transform[] _ropeParts;
     [SerializeField] private Transform _rotateBody;
+    [SerializeField] private Transform _leftRopePivot;
+    [SerializeField] private Transform _rightRopePivot;
 
     [SerializeField] private float _maxAngleX = -70;
     [SerializeField] private float _totalDuration = 2;
@@ -17,29 +19,33 @@ public class BallistaAnimator : MonoBehaviour
 
     private const float FullAngle = 360;
     private const int Direction = -1;
-    
+
     public void AnimateRopeDisappearance()
     {
-        Sequence ropeSequence = DOTween.Sequence().
-            OnKill(() => _ropeParts.ToList().ForEach(i => i.gameObject.transform.localScale = Vector3.one * 0.1f));
+        Sequence ropeSequence = DOTween.Sequence();
 
         int count = _ropeParts.Length;
-        float individualShrinkDuration = 0.3f;
+        float totalRopeTime = _totalDuration * 0.75f;
 
-        for (int i = count - 1; i >= 0; i--)
+        for (int i = 0; i < count; i++) 
         {
             Transform part = _ropeParts[i];
+            part.localScale = Vector3.one * 0.1f;
 
             float progress = (float)i / (count > 1 ? count - 1 : 1);
 
-            float easedProgress = 1f - (1f - progress) * (1f - progress);
+            float easedProgress = progress * progress;
 
-            float maxDelay = _totalDuration - individualShrinkDuration;
-            float delay = easedProgress * maxDelay;
+            // Если хочешь еще более резкое замедление в конце, используй InCubic:
+            // float easedProgress = progress * progress * progress;
+
+            float delay = easedProgress * (totalRopeTime - 0.2f);
+
+            float shrinkTime = Mathf.Lerp(2, 0.05f, progress);
 
             ropeSequence.Insert(delay,
-                part.DOScale(Vector3.zero, individualShrinkDuration)
-                    .SetEase(Ease.OutQuad)
+                part.DOScale(Vector3.zero, shrinkTime)
+                    .SetEase(Ease.OutQuad) 
             );
         }
     }
@@ -60,6 +66,8 @@ public class BallistaAnimator : MonoBehaviour
         _finalRotateBody = new Vector3(_maxAngleX, 0, 0);
 
         _rotateBody.transform.eulerAngles = new Vector3(-90, 0, 0);
+        _leftRopePivot.transform.eulerAngles = new Vector3(0, 0, -37.5f);
+        _rightRopePivot.transform.eulerAngles = new Vector3(0, 0, 0);
 
         _backMechRoll.DOLocalRotate(_finalRotateBackMechRoll, _totalDuration, RotateMode.FastBeyond360)
             .SetRelative(true)
@@ -70,5 +78,11 @@ public class BallistaAnimator : MonoBehaviour
             .SetRelative(true)
             .SetEase(Ease.OutQuart)
             .From(_rotateBody.localEulerAngles);
+
+        _leftRopePivot.DOLocalRotate(Vector3.zero, _totalDuration)
+            .SetEase(Ease.OutQuart);
+
+        _rightRopePivot.DOLocalRotate(new Vector3(0, 0, -37.5f), _totalDuration)
+            .SetEase(Ease.OutQuart);
     }
 }
