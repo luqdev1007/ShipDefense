c914cd24ca1aab119b1753adfea29424111ca666
LuQmu5 <luqdev1007@gmail.com>
2026-01-23 22:19:03 +0400

    base anim

--- Assets/BallistaAnimator.cs
+++ Assets/BallistaAnimator.cs
@@ -1,5 +1,6 @@
 using DG.Tweening;
 using System.Collections;
+using System.Linq;
 using UnityEngine;
 
 public class BallistaAnimator : MonoBehaviour
@@ -16,58 +17,58 @@ public class BallistaAnimator : MonoBehaviour
 
     private const float FullAngle = 360;
     private const int Direction = -1;
-    private const int InfinityLoops = -1;
-
-
-    public IEnumerator AnimateRopeDisappearance()
+    
+    public void AnimateRopeDisappearance()
     {
-        while (true)
+        Sequence ropeSequence = DOTween.Sequence().
+            OnKill(() => _ropeParts.ToList().ForEach(i => i.gameObject.transform.localScale = Vector3.one * 0.1f));
+
+        int count = _ropeParts.Length;
+        float individualShrinkDuration = 0.3f;
+
+        for (int i = count - 1; i >= 0; i--)
         {
-            Sequence ropeSequence = DOTween.Sequence();
-            int count = _ropeParts.Length;
-            float individualShrinkDuration = 0.3f;
+            Transform part = _ropeParts[i];
 
-            for (int i = count - 1; i >= 0; i--)
-            {
-                Transform part = _ropeParts[i];
+            float progress = (float)i / (count > 1 ? count - 1 : 1);
 
-                float progress = (float)i / (count > 1 ? count - 1 : 1);
+            float easedProgress = 1f - (1f - progress) * (1f - progress);
 
-                float easedProgress = 1f - (1f - progress) * (1f - progress);
+            float maxDelay = _totalDuration - individualShrinkDuration;
+            float delay = easedProgress * maxDelay;
 
-                float maxDelay = _totalDuration - individualShrinkDuration;
-                float delay = easedProgress * maxDelay;
-
-                ropeSequence.Insert(delay,
-                    part.DOScale(Vector3.zero, individualShrinkDuration)
-                        .SetEase(Ease.OutQuad)
-                );
-            }
-
-            yield return new WaitUntil(() => ropeSequence.IsComplete());
-
-            ropeSequence.Kill();
+            ropeSequence.Insert(delay,
+                part.DOScale(Vector3.zero, individualShrinkDuration)
+                    .SetEase(Ease.OutQuad)
+            );
         }
     }
 
-
-    public void Start()
+    private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayAnimation();
+        }
+    }
+
+    public void PlayAnimation()
+    {
+        AnimateRopeDisappearance();
+
         _finalRotateBackMechRoll = new Vector3(FullAngle * Direction, 0, 0);
         _finalRotateBody = new Vector3(_maxAngleX, 0, 0);
+
+        _rotateBody.transform.eulerAngles = new Vector3(-90, 0, 0);
 
         _backMechRoll.DOLocalRotate(_finalRotateBackMechRoll, _totalDuration, RotateMode.FastBeyond360)
             .SetRelative(true)
             .SetEase(Ease.OutQuart)
-            .SetLoops(InfinityLoops)
             .From(_backMechRoll.localEulerAngles);
 
         _rotateBody.DOLocalRotate(_finalRotateBody, _totalDuration)
             .SetRelative(true)
             .SetEase(Ease.OutQuart)
-            .SetLoops(InfinityLoops)
             .From(_rotateBody.localEulerAngles);
-
-        StartCoroutine(AnimateRopeDisappearance());      
     }
 }
