d52a35d56f00a972474c25c25956a023d82bc743
LuQmu5 <luqdev1007@gmail.com>
2026-02-03 18:56:15 +0400

    rework

--- Assets/_Project/Art/Models/Ballista/BallistaAnimator.cs
+++ Assets/_Project/Art/Models/Ballista/BallistaAnimator.cs
@@ -1,60 +1,16 @@
 using UnityEngine;
-using DG.Tweening;
 
 public class BallistaAnimator : MonoBehaviour
 {
-    [SerializeField] private Animator _animator;
-    [SerializeField] private float _chargeDuration = 2f;
-    [SerializeField] private float _reverseSpeedMultiplier = 2f;
-    private Tween _chargeTween;
-    private float _currentProgress = 0f;
-    private bool _isReversing = false;
-
-    private const string ParamName = "ChargeProgress";
-
-    private void Update()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0) && !_isReversing)
-        {
-            StartCharge();
-        }
-
-        if (Input.GetKeyUp(KeyCode.Mouse0))
-        {
-            StartReverse();
-        }
+        
     }
 
-    private void StartCharge()
+    // Update is called once per frame
+    void Update()
     {
-        _chargeTween?.Kill();
-
-        float remainingTime = _chargeDuration * (1f - _currentProgress);
-
-        _chargeTween = DOTween.To(() => _currentProgress, x => _currentProgress = x, 1f, remainingTime)
-            .SetEase(Ease.Linear)
-            .OnUpdate(() =>
-            {
-                _animator.SetFloat(ParamName, _currentProgress);
-            });
-    }
-
-    private void StartReverse()
-    {
-        _chargeTween?.Kill();
-        _isReversing = true;
-
-        float reverseDuration = (Mathf.Sqrt(_chargeDuration) / _reverseSpeedMultiplier) * _currentProgress;
-
-        _chargeTween = DOTween.To(() => _currentProgress, x => _currentProgress = x, 0f, reverseDuration)
-            .SetEase(Ease.OutBack)
-            .OnUpdate(() =>
-            {
-                _animator.SetFloat(ParamName, _currentProgress);
-            })
-            .OnComplete(() =>
-            {
-                _isReversing = false;
-            });
+        
     }
 }
